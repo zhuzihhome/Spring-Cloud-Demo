446a5ba55fec004b11be6eeb2b20002875b78ec6
zhuzihui <892386148@qq.com>
2018-10-16 21:49:07 +0800

    chapter110602

---
+++ chapter110602/chapter110602-zipkin-server/src/main/java/com/froad/Application.java
@@ -0,0 +1,21 @@
+package com.froad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * @author ZHUZIHUI
+ * @date 2018年10月15日
+ */
+@EnableZipkinStreamServer
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
