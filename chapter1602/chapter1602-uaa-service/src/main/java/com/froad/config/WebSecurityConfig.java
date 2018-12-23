d863ab504000212d19d98f1b000171e6ce9bf097
zhuzihui <892386148@qq.com>
2018-12-23 21:52:38 +0800

    update

--- chapter1602/chapter1602-uaa-service/src/main/java/com/froad/config/WebSecurityConfig.java
+++ chapter1602/chapter1602-uaa-service/src/main/java/com/froad/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -19,7 +18,6 @@ import com.froad.service.security.UserDetailsServiceImpl;
  * @date 2018年10月28日
  */
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
